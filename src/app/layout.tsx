210ff926ec96c5eab411fd97baa430af38ee0290
Dann Wee <dannweezj@gmail.com>
2024-10-31 13:36:20 +0800

    feat: updated ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layout/navbar";
 import ContextProvider from "@/context";
 import { headers } from "next/headers";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Bid It",
@@ -31,7 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} overflow-hidden `}
+        suppressHydrationWarning={true}
       >
         <ContextProvider cookies={cookies}>
           <Navbar />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
+import PageContainer from "@/components/ui/page-container";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 
-export default function DashboardPage() {
-  return <div></div>;
+export default function Home() {
+  return (
+    <main className="pt-24">
+      <PageContainer scrollable={true}>
+        <div className="space-y-2">
+          <div className="flex items-center justify-between space-y-2">
+            <h2 className="text-2xl font-bold tracking-tight">
+              Dutch Auction for Token ICOs
+            </h2>
+          </div>
+          <Tabs defaultValue="dashboard" className="space-y-4">
+            <TabsList>
+              <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
+              <TabsTrigger value="create-auction">Create Auction</TabsTrigger>
+              <TabsTrigger value="profile">Profile</TabsTrigger>
+            </TabsList>
+            <TabsContent value="dashboard" className="space-y-4">
+              Dashboard
+            </TabsContent>
+            <TabsContent value="create-auction" className="space-y-4">
+              Create Auction
+            </TabsContent>
+            <TabsContent value="profile" className="space-y-4">
+              Profile
+            </TabsContent>
+          </Tabs>
+        </div>
+      </PageContainer>
+    </main>
+  );
 }
