c35a19f5098e4e472ee443f6ebc16d8ee95336c9
Dann Wee <dannweezj@gmail.com>
2024-11-01 00:55:54 +0800

    feat: added a linechart card template

--- src/components/layout/dashboard.tsx
+++ src/components/layout/dashboard.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { AuctionCard } from "../ui/auction-card";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <div className="flex items-center justify-between space-y-2 mb-6">
+        <h2 className="text-2xl font-bold tracking-tight">Live Auctions</h2>
+      </div>
+      <AuctionCard />
+    </div>
+  );
 };
 
 export default Dashboard;
