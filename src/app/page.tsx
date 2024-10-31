23cd0e6bc237792ec6d1acf26658f26bb44ba86a
Dann Wee <dannweezj@gmail.com>
2024-10-31 14:54:48 +0800

    feat: ui updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,29 @@ import PageContainer from "@/components/ui/page-container";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Dashboard from "@/components/layout/dashboard";
+import Profile from "@/components/layout/profile";
 
 export default function Home() {
   return (
     <main className="pt-24">
       <PageContainer scrollable={true}>
         <div className="space-y-2">
-          <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-2xl font-bold tracking-tight">
-              Dutch Auction for Token ICOs
-            </h2>
-            <Button className="py-2 px-5 bg-black text-white font-light rounded-xl hover:bg-black/70 hover:scale-105 transition ease-in-out disabled:bg-black-opacity-30">
-              Create Auction
-            </Button>
-          </div>
           <Tabs defaultValue="dashboard" className="space-y-4">
-            <TabsList>
-              <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
-              <TabsTrigger value="profile">Profile</TabsTrigger>
-            </TabsList>
+            <div className="flex items-center justify-between space-y-2">
+              <TabsList>
+                <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
+                <TabsTrigger value="profile">Profile</TabsTrigger>
+              </TabsList>
+              <Button className="py-2 px-5 bg-black text-white font-light rounded-xl hover:bg-black/70 hover:scale-105 transition ease-in-out disabled:bg-black-opacity-30">
+                Create Auction
+              </Button>
+            </div>
             <TabsContent value="dashboard" className="space-y-4">
-              Dashboard
+              <Dashboard />
             </TabsContent>
             <TabsContent value="profile" className="space-y-4">
-              Profile
+              <Profile />
             </TabsContent>
           </Tabs>
         </div>
