9d11bda4f7b004622cc025cce40dde67d850d793
Dann Wee <dannweezj@gmail.com>
2024-10-31 13:43:28 +0800

    feat: update ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import PageContainer from "@/components/ui/page-container";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -11,18 +12,17 @@ export default function Home() {
             <h2 className="text-2xl font-bold tracking-tight">
               Dutch Auction for Token ICOs
             </h2>
+            <Button className="py-2 px-5 bg-black text-white font-light rounded-xl hover:bg-black/70 hover:scale-105 transition ease-in-out disabled:bg-black-opacity-30">
+              Create Auction
+            </Button>
           </div>
           <Tabs defaultValue="dashboard" className="space-y-4">
             <TabsList>
               <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
-              <TabsTrigger value="create-auction">Create Auction</TabsTrigger>
               <TabsTrigger value="profile">Profile</TabsTrigger>
             </TabsList>
             <TabsContent value="dashboard" className="space-y-4">
               Dashboard
-            </TabsContent>
-            <TabsContent value="create-auction" className="space-y-4">
-              Create Auction
             </TabsContent>
             <TabsContent value="profile" className="space-y-4">
               Profile
